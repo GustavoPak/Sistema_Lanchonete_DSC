3f4400274e2a7f879aee485c9fb54e6d3bd841c4
GustavoPak <gutskinaruto159@gmail.com>
2026-04-24 10:53:28 -0300

    Adicionando arquivos do projeto

---
+++ src/model/NewEntity.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author gutsk
+ */
+public class NewEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NewEntity)) {
+            return false;
+        }
+        NewEntity other = (NewEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.NewEntity[ id=" + id + " ]";
+    }
+    
+}
