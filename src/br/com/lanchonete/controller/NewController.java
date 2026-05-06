b003ec140db5ab575ada385e758c03dcd9c6529a
GustavoPak <gutskinaruto159@gmail.com>
2026-05-06 11:08:04 -0300

    Refatorando a estrutura

--- src/br/com/lanchonete/controller/NewController.java
+++ src/br/com/lanchonete/controller/NewController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package util;
+package br.com.lanchonete.controller;
 
 /**
  *
  * @author gutsk
  */
-public class utils {
+public class NewController {
     
 }
